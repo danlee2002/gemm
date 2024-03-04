4c4d13619b0b988a80a7e36e34d835157723e04f
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-03-04 16:16:13 -0500

    added naive approach for benchmark purposes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gemm
+- The following repository aims to optimize gemm operations, utilizng cache locality and multi threading
  
--- gemm.c
+++ gemm.c
@@ -1,14 +1,16 @@
 #include <assert.h>
 #include <immintrin.h>
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
-#define N 1024
+#define N 2048
 #define BLOCK 4
 float A[N][N];
 // assuming B[n][n] is transposed
 float B[N][N];
 float C[N][N];
+float transposedBlock[N][N];
 
 uint64_t nanos() {
   struct timespec start;
@@ -19,7 +21,29 @@ uint64_t nanos() {
 int main() {
   assert(N % BLOCK == 0);
   uint64_t start = nanos();
+  for (int y = 0; y < N; y++) {
+    for (int x = 0; x < N; x++) {
+      float acc = 0;
+      for (int k = 0; k < N; k++) {
+        acc += A[y][k] * B[k][x];
+      }
+      C[x][y] = acc;
+    }
+  }
+  uint64_t end = nanos();
 
+  printf("%lld %lld\n", start, end);
+  double s = (end - start) * 1e-9;
+  double gflop = (2.0 * N * N * N) * 1e-9;
+  printf("%0.2f GFLOP/s\n", gflop / s);
+
+  start = nanos();
+  // transpose matrix in c
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      transposedBlock[i][j] = B[j][i];
+    }
+  }
   for (int by = 0; by < N; by += BLOCK) {
     for (int bx = 0; bx < N; bx += BLOCK) {
       float tc[BLOCK][BLOCK];
@@ -28,12 +52,11 @@ int main() {
           float acc = 0;
           // transposed dot product
           for (int k = 0; k < N; k++) {
-            acc += A[by + y][k] * B[bx + x][k];
+            acc += A[by + y][k] * transposedBlock[bx + x][k];
           }
           tc[y][x] = acc;
         }
       }
-
       for (int y = 0; y < BLOCK; y++) {
         for (int x = 0; x < BLOCK; x++) {
           C[bx + y][bx + x] = tc[y][x];
@@ -41,11 +64,10 @@ int main() {
       }
     }
   }
-
-  uint64_t end = nanos();
+  end = nanos();
   printf("%lld %lld\n", start, end);
-  double s = (end - start) * 1e-9;
-  double gflop = (2.0 * N * N * N) * 1e-9;
+  s = (end - start) * 1e-9;
+  gflop = (2.0 * N * N * N) * 1e-9;
   printf("%0.2f GFLOP/s\n", gflop / s);
   return 0;
 }
